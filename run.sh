943105ba12f57b1a8f1365b82abaa123aefc7b0f
Heedong Goh <wellposed@gmail.com>
2016-07-17 15:23:49 -0500

    update run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 TITLE="TEST"
+CPUSIZE=20
+
 PETSC_DIR=/software/petsc/petsc-3.6.3
 PETSC_ARCH=arch-icc-opt
-CPUSIZE=20
+
+MPIEXE=$PETSC_DIR/bin/petscmpiexec
 EXE=./bin/run
 
 case $1 in
     nohup )
-	nohup $PETSC_DIR/bin/petscmpiexec -n $CPUSIZE $EXE -title $TITLE > $TITLE.log 2>&1 &
+	nohup $MPIEXE -n $CPUSIZE $EXE -title $TITLE > $TITLE.log 2>&1 &
 	echo $TITLE.log "is created." ;;
     * )
-	$PETSC_DIR/bin/petscmpiexec -n $CPUSIZE $EXE -title $TITLE ;;
+	$MPIEXE -n $CPUSIZE $EXE -title $TITLE ;;
 esac
 
 exit $?
