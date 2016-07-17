58e8264c8281da37f23e0ddb6dac6cc5ce819268
Heedong Goh <wellposed@gmail.com>
2016-07-16 20:32:21 -0500

    update shell script

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
 export PETSC_DIR=/software/petsc/petsc-3.6.3
 export PETSC_ARCH=arch-icc-opt
 
 cd ./bin/
 
-/software/petsc/petsc-3.6.3/bin/petscmpiexec -n 5 ./run 
+/software/petsc/petsc-3.6.3/bin/petscmpiexec -n 4 ./run 
+
+exit $?
