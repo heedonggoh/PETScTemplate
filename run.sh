7dbe93949d9d0126b0522f56569c4d52e4d7de17
Heedong Goh <wellposed@gmail.com>
2016-07-17 18:39:06 -0500

    modify run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,8 @@
 ## PROGRAM AND ITS ARGUMENTS
 EXE=./bin/run
 CPUSIZE=20
-TITLE=TEST
+TITLE=test
+ARG="-title $TITLE -log_summary $TITLE.log"
 ## PETSC ARGUMENTS
 PETSC_DIR=/software/petsc/petsc-3.6.3
 PETSC_ARCH=arch-icc-opt
@@ -13,10 +14,10 @@ MPIEXE=$PETSC_DIR/bin/petscmpiexec
 ## DO NOT CHANGE ########################################################
 case $1 in
     nohup )
-	nohup $MPIEXE -n $CPUSIZE $EXE -title $TITLE > $TITLE.log 2>&1 &
-	echo $TITLE.log "is created." ;;
+	nohup $MPIEXE -n $CPUSIZE $EXE $ARG > $TITLE.out 2>&1 &
+	echo $TITLE.out "is created." ;;
     * )
-	$MPIEXE -n $CPUSIZE $EXE -title $TITLE ;;
+	$MPIEXE -n $CPUSIZE $EXE $ARG ;;
 esac
 exit $?
 #########################################################################
