c335bcf0de07f7f54292f7812f33cc3743a4e46e
Heedong Goh <wellposed@gmail.com>
2016-07-17 13:24:50 -0500

    add nohup option

--- run.sh
+++ run.sh
@@ -6,5 +6,12 @@ PETSC_ARCH=arch-icc-opt
 CPUSIZE=20
 EXE=./bin/run
 
-$PETSC_DIR/bin/petscmpiexec -n $CPUSIZE $EXE -title $TITLE
+case $1 in
+    nohup )
+	nohup $PETSC_DIR/bin/petscmpiexec -n $CPUSIZE $EXE -title $TITLE > $TITLE.log 2>&1 &
+	echo $TITLE.log "is created." ;;
+    * )
+	$PETSC_DIR/bin/petscmpiexec -n $CPUSIZE $EXE -title $TITLE ;;
+esac
+
 exit $?
