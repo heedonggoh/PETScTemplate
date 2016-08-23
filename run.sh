c59fc7c11a16eca64096977dcba1b924ee5caec2
Heedong Goh <wellposed@gmail.com>
2016-08-23 15:41:08 -0500

    add help option in run.sh

--- run.sh
+++ run.sh
@@ -13,10 +13,17 @@ export PETSC_ARCH=arch-icc-opt
 export MPIEXE=$PETSC_DIR/bin/petscmpiexec
 ## DO NOT CHANGE ########################################################
 case $1 in
-    nohup )
+    --help )
+	echo "Usage: bash run.sh [OPTION]"
+	echo " "
+	echo "  --nohup   ignore HUP signal"
+	echo "  --help    display this help and exit" ;;
+    --nohup )
+	echo "nohup $MPIEXE -n $CPUSIZE $EXE $ARG > $TITLE.out 2>&1 &"
 	nohup $MPIEXE -n $CPUSIZE $EXE $ARG > $TITLE.out 2>&1 &
-	echo $TITLE.out "is created." ;;
+	echo "$TITLE.out is created." ;;
     * )
+	echo "$MPIEXE -n $CPUSIZE $EXE $ARG"
 	$MPIEXE -n $CPUSIZE $EXE $ARG ;;
 esac
 exit $?
