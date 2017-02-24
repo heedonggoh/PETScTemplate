a954a76be4057536acdb88d191dfe8912ae30b1b
Heedong Goh <wellposed@gmail.com>
2017-02-24 08:25:19 -0600

    creat branch for petsc-3.7.5

--- run.sh
+++ run.sh
@@ -6,11 +6,10 @@
 EXE=./bin/run
 CPUSIZE=2
 TITLE=test
-#ARG="-title $TITLE -log_summary $TITLE.log"
 ARG="-title $TITLE -log_view :$TITLE.log"
 ## PETSC AND MPIEXE PATHS
-export PETSC_DIR=/opt/petsc/petsc-3.7.3
-export PETSC_ARCH=arch-gcc-opt
+export PETSC_DIR=/terra3/heedong/software/petsc-3.7.5
+export PETSC_ARCH=arch-icc-opt
 export MPIEXE=$PETSC_DIR/bin/petscmpiexec
 ## DO NOT CHANGE ########################################################
 case $1 in
@@ -18,11 +17,15 @@ case $1 in
 	echo "Usage: bash run.sh [OPTION]"
 	echo " "
 	echo "  --nohup   ignore HUP signal"
+	echo "  --clean   remove .log and .out" 
 	echo "  --help    display this help and exit" ;;
     --nohup )
 	echo "nohup $MPIEXE -n $CPUSIZE $EXE $ARG > $TITLE.out 2>&1 &"
 	nohup $MPIEXE -n $CPUSIZE $EXE $ARG > $TITLE.out 2>&1 &
 	echo "$TITLE.out is created." ;;
+    --clean )
+	echo "rm $TITLE.log $TITLE.out"
+	rm $TITLE.log $TITLE.out ;;
     * )
 	echo "$MPIEXE -n $CPUSIZE $EXE $ARG"
 	$MPIEXE -n $CPUSIZE $EXE $ARG ;;
